8a431889f81da4f8d1ef636780053f614125ad4c
fathtria <57378148+fathtria@users.noreply.github.com>
2019-11-05 13:46:37 +0700

    Update maingit.java

--- maingit.java
+++ maingit.java
@@ -5,7 +5,10 @@ public class maingit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			System.out.println("Version7");
-			System.out.println("deviece7");
+			System.out.println("device8");
+			System.out.println("Aldi Ganteng");
+			System.out.println("Versi 7.1");
+			
 	}
 
 }
