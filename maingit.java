505608725dca45633a9c972dfd1e6d7746806047
Devi Irma Vianti <devimav@gmail.com>
2019-11-05 10:45:13 +0700

    Update maingit.java

--- maingit.java
+++ maingit.java
@@ -4,8 +4,8 @@ public class maingit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			System.out.println("Version5");
-			System.out.println("Hello World5");
+			System.out.println("Version6");
+			System.out.println("Hello World6");
 	}
 
 }
